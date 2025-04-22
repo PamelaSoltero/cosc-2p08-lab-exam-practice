a1beef109c3bb37bc88126b14c8530e7c555bf2e
Pamela Soltero <pamela.soltero@outlook.com>
2025-04-21 21:58:56 -0400

    Completed Lab Exam Practice

--- student_analysis.py
+++ student_analysis.py
@@ -15,8 +15,19 @@ class StudentAnalysis:
     - Convert Yes/No columns ('part_time_job', 'extracurricular_participation') to 1/0
     """
     def clean_data(self):
-        ## ADD YOUR CODE HERE
-        pass # Remove this pass when you add code
+        self.df.drop(['student_id'], axis =1, inplace = True)
+        
+        num_cols = ['age', 'exam_score', 'study_hours_per_day', 'social_media_hours', 'netflix_hours']
+        for col in num_cols:
+            median_value = self.df[col].median()
+            self.df[col].fillna(median_value, inplace=True)
+        
+        most_common_edu = self.df['parental_education_level'].mode()[0]
+        self.df['parental_education_level'].fillna(most_common_edu, inplace= True)
+
+        self.df['part_time_job']= self.df['part_time_job'].map({"Yes" : 1, "No":0})
+        self.df['extracurricular_participation'] = self.df['extracurricular_participation'].map({"Yes" : 1, "No":0})
+        
 
     """ Data Transformation
     - Create a new column 'total_leisure_hours' that combines time spent on social media and Netflix
@@ -25,71 +36,77 @@ class StudentAnalysis:
     - Create a new column 'parental_education_numeric' that converts education levels into numerical values: 'None': 0, 'High School': 1, 'Bachelor': 2, 'Master': 3
     """
     def transform_data(self):
-        ## ADD YOUR CODE HERE
+        self.df['total_leisure_hours'] = self.df['social_media_hours'] + self.df['netflix_hours']
+        self.df['study_efficiency'] = self.df['study_hours_per_day'] / self.df['exam_score']
+        self.df['exam_score'][self.df['study_hours_per_day'] == 0 ] = 0
+        self.df['age_group'] = "<18"
+        self.df['age_group'][self.df['age'] >= 18] = "18-21"
+        self.df['age_group'][self.df['age'] >= 22] = "22+"
+        self.df['parental_education_numeric'] = self.df['parental_education_level'].map({'None': 0, 'High School': 1, 'Bachelor': 2, 'Master': 3})
         pass # Remove this pass when you add code
 
     """ Analysis Functions
     - Returns descriptive statistics of the dataset
     """
     def get_summary(self):
-        return ## ADD YOUR CODE HERE
+        return self.df.describe()
 
     """ Analysis Functions
     - Identifies and returns the count of missing values per column
     """
     def get_missing_values(self):
-        return ## ADD YOUR CODE HERE
+        return self.df.isna().sum()
 
     """ Analysis Functions
     - Return the mean of the exam scores
     """
     def get_score_mean(self):
-        return ## ADD YOUR CODE HERE
+        return self.df['exam_score'].mean()
 
     """ Analysis Functions
     - Computes average (mean) exam scores based on grouping by a given column
     """
     def score_by_column(self, column):
-        return ## ADD YOUR CODE HERE
+        return self.df.groupby(column)['exam_score'].mean()
 
     """ Analysis Functions
     - Returns the average (mean) exam score for each unique number of study hours
     """
     def score_by_study_hours(self):
-        return ## ADD YOUR CODE HERE
+        return self.df.groupby('study_hours_per_day')['exam_score'].mean()
 
     """ Analysis Functions
     - Returns the count of each unique value in the age column
     """
     def get_age_value_counts(self):
-        return ## ADD YOUR CODE HERE
+        return self.df.groupby('age')['age'].size()
 
     """ Analysis Functions
     - Returns the count of each unique value in the study_hours_per_day column
     """
     def get_study_hours_value_counts(self):
-        return ## ADD YOUR CODE HERE
+        return self.df.groupby('study_hours_per_day')['study_hours_per_day'].size()
 
     """ Analysis Functions
     - Returns the minimum exam score
     """
     def get_min_score(self):
-        return ## ADD YOUR CODE HERE
+        return self.df['exam_score'].min()
 
     """ Analysis Functions
     - Returns the maximum exam score
     """
     def get_max_score(self):
-        return ## ADD YOUR CODE HERE
+        return self.df['exam_score'].max()
 
     """ Analysis Functions
     - Returns the sum of all study hours
     """
     def get_total_study_hours(self):
-        return ## ADD YOUR CODE HERE
+        return self.df['study_hours_per_day'].sum()
 
     """ Analysis Functions
     - Returns the sum of all social media hours
     """
     def get_total_social_media_hours(self):
-        return ## ADD YOUR CODE HERE
+        return self.df['social_media_hours'].sum()
